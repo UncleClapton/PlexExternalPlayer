6eb1b922e3be9ce4bef2d9623788d24175e42abe
UncleClapton <camjwelt@gmail.com>
2016-01-22 03:40:12 -0500

    Implement variables and Generic protocol.

--- Agent/PlexExternalPlayerAgent/Program.cs
+++ Agent/PlexExternalPlayerAgent/Program.cs
@@ -28,45 +28,102 @@ namespace PlexExternalPlayerAgent
                 httpServer.Use(new TcpListenerAdapter(new TcpListener(IPAddress.Loopback, 7251)));
                 httpServer.Use((context, next) =>
                 {
-                    Console.WriteLine("Got Request!");
-
                     var protocol = context.Request.QueryString.GetByName("protocol");
 
-                    if (protocol != Properties.Resources.ExpectedProtocol)
+                    if (protocol == Properties.Resources.PlexProtocol)
                     {
-                        MessageBox.Show($"Agent and script version differ.  Agent: {Properties.Resources.ExpectedProtocol}  Script : {protocol}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        #region PlexProtocol
+
+                        Console.WriteLine("Recieved Plex Request!");
+
+                        context.Response = HttpResponse.CreateWithMessage(HttpResponseCode.Ok, "Received", false);
+
+                        var streamPath = WebUtility.UrlDecode(context.Request.QueryString.GetByName("url"));
+                        var id = context.Request.QueryString.GetByName("id") ?? "";
+                        var title = context.Request.QueryString.GetByName("title") ?? "";
+                        var grandparentTitle = context.Request.QueryString.GetByName("grandfatherTitle") ?? "";
+                        var rating = context.Request.QueryString.GetByName("rating") ?? "";
+                        var filePath = WebUtility.UrlDecode(context.Request.QueryString.GetByName("filePath")) ?? "";
+
+                        var playerArguments = Properties.Settings.Default.PlayerPlexArguments
+                                                .Replace("%url%", streamPath)
+                                                .Replace("%fileId%", id)
+                                                .Replace("%title%", title)
+                                                .Replace("%seriesTitle%", grandparentTitle )
+                                                .Replace("%fullTitle%", (string.IsNullOrWhiteSpace(grandparentTitle) ? "" : grandparentTitle + " - ") + title)
+                                                .Replace("%contentRating%", rating)
+                                                .Replace("%filePath%", filePath);
+
+                        if (!string.IsNullOrWhiteSpace(streamPath))
+                        {
+                            try
+                            {
+                                var info = new ProcessStartInfo();
+                                info.FileName = Properties.Settings.Default.PlayerPath;
+                                info.Arguments = playerArguments;
+                                info.UseShellExecute = Properties.Settings.Default.ShowCommandLine;
+                                Process.Start(info);
+                            }
+                            catch (Exception e)
+                            {
+                                MessageBox.Show($"Error running {streamPath}  due to : {e.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Blank or null url recieived, Unable to open");
+                        }
+                            
+                 
                         return Task.Factory.GetCompleted();
+                        #endregion
                     }
-
-                    context.Response = HttpResponse.CreateWithMessage(HttpResponseCode.Ok, "Received", false);
-
-                    var streamPath = WebUtility.UrlDecode(context.Request.QueryString.GetByName("item"));
-                    if (streamPath.EndsWith(".avi") ||
-                        streamPath.EndsWith(".mkv") ||
-                        streamPath.EndsWith(".mp4") ||
-                        streamPath.EndsWith(".mpg") ||
-                        streamPath.EndsWith(".ts") ||
-                        streamPath.EndsWith(".mpeg"))
+                    else if (protocol == Properties.Resources.GenericProtocol)
                     {
-                        try
+                        #region GenericProtocol
+
+                        Console.WriteLine("Recieved Generic Request!");
+
+                        context.Response = HttpResponse.CreateWithMessage(HttpResponseCode.Ok, "Received", false);
+
+                        var streamPath = WebUtility.UrlDecode(context.Request.QueryString.GetByName("url"));
+                        var title = context.Request.QueryString.GetByName("title") ?? "";
+
+                        var playerArguments = Properties.Settings.Default.PlayerPlexArguments
+                                                .Replace("%url%", streamPath)
+                                                .Replace("%title%", title);
+
+                        if (!string.IsNullOrWhiteSpace(streamPath))
                         {
-                            var info = new ProcessStartInfo();
-                            info.FileName = Properties.Settings.Default.PlayerPath;
-                            info.Arguments = streamPath + Properties.Settings.Default.PlayerArguments;
-                            info.UseShellExecute = Properties.Settings.Default.ShowCommandLine;
-                            Process.Start(info);
+                            try
+                            {
+                                var info = new ProcessStartInfo();
+                                info.FileName = Properties.Settings.Default.PlayerPath;
+                                info.Arguments = playerArguments;
+                                info.UseShellExecute = Properties.Settings.Default.ShowCommandLine;
+                                Process.Start(info);
+                            }
+                            catch (Exception e)
+                            {
+                                MessageBox.Show($"Error running {streamPath}  due to : {e.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
                         }
-                        catch (Exception e)
+                        else
                         {
-                            MessageBox.Show($"Error running {streamPath}  due to : {e.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            MessageBox.Show($"Blank or null url recieived, Unable to open");
                         }
+
+
+                        return Task.Factory.GetCompleted();
+                        #endregion
                     }
                     else
                     {
-                        MessageBox.Show($"Tried to run {streamPath} but it wasn't allowed.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
+                        Console.WriteLine("Recieved Invalid Request!");
 
-                    return Task.Factory.GetCompleted();
+                        MessageBox.Show($"Invalid protocol recieived: {protocol}. Ensure the sending script is updated and valid.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return Task.Factory.GetCompleted();
+                    }
                 });
 
                 httpServer.Start();
